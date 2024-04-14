ec525a6041dc8908a23feac115328cbaf9b2ef3e
Dan O'Neil <danoneil27@gmail.com>
2024-04-14 19:45:11 +0100

    Updated some links, added tailwind & icons

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Dan\'s Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const GITHUB_URL = 'https://github.com/djoneil';
+export const TWITTER_URL = 'https://twitter.com/itsdanoneil';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/djoneil/';
