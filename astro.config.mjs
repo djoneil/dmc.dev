ca4a1120a59d892bb32874aeebbe794617ac1360
Dan O'Neil <danoneil27@gmail.com>
2024-04-21 15:51:48 +0100

    Added github actions to deploy to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://djoneil.githubl.io',
+  base: 'dmc.dev',
   integrations: [mdx(), sitemap(), tailwind(), icon()]
 });
